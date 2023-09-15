14a051e5b0fbf9b89c4b06a20940ca7b490372d1
Bryce <99645139+bcgrahn@users.noreply.github.com>
2023-09-16 01:35:43 +0200

    manual simulation

--- Multi_agent/config_files/custom_reward.py
+++ Multi_agent/config_files/custom_reward.py
@@ -5,8 +5,10 @@ def my_reward_fn(traffic_signal):
     diff_wait = traffic_signal._diff_waiting_time_reward() # Default reward
     diff_avg_speed = traffic_signal.diff_avg_speed_reward() 
     diff_pressure = traffic_signal.diff_pressure_reward()
-    reward = 1*diff_wait + 1*diff_avg_speed + 0.02*diff_pressure
+    # print(f'Diff wait: {diff_wait}, Diff speed: {diff_avg_speed}, Diff pressure: {diff_pressure}')
+    
+    reward = 1*diff_wait + 1*diff_avg_speed # + 0.02*diff_pressure # Don't include diff pressure. It overshadows the other rewards
     # Implement exponetial punishement for waiting cars
     #Implement punishment if a phase hasn't been implemented in a while.
-
+    # Implement reward for cars that never stopped at the robot
     return reward
