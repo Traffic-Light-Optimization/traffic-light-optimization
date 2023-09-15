e8a9f242f8dd5941f2151814a1bd81c7a8a7cf56
Michael Rolle <michaelrolle7@gmail.com>
2023-09-15 11:49:59 +0200

    Cologne8 greedy implemented

--- Greedy_Simulation.py
+++ Greedy_Simulation.py
@@ -2,12 +2,12 @@ import sumo_rl
 from fixed_control_configs.greedy.observation import GreedyObservationFunction
 from fixed_control_configs.greedy.action_lane_relationships import Map_Junction_Action_Lanes
 
-map = "cologne3" #NB, don't forget to change this variable if you change the network, see fixed_control_configs/action_lane_relationships for map names
+map = "cologne8" #NB, don't forget to change this variable if you change the network, see fixed_control_configs/action_lane_relationships for map names
 action_lanes = Map_Junction_Action_Lanes[map]
 
 env = sumo_rl.env(
-    net_file="Multi_agent/nets/cologne3/cologne3.net.xml",
-    route_file="Multi_agent/nets/cologne3/cologne3.rou.xml",
+    net_file="Multi_agent/nets/cologne8/cologne8.net.xml",
+    route_file="Multi_agent/nets/cologne8/cologne8.rou.xml",
     use_gui=True,
     num_seconds=3600,
     delta_time=5,
@@ -31,14 +31,14 @@ done = False
 while not done:
     for agent in env.agent_iter():
         observation, reward, termination, truncation, info = env.last()
-        action = env.action_space(agent).sample() if not done else None
+        # action = env.action_space(agent).sample() if not done else None
         if agent in action_lanes.keys():
             action = choose_action(observation, agent) if not done else None
         else:
-            # raise ValueError(f"Agent {agent} has not been implemented in the config file")
+            raise ValueError(f"Agent {agent} has not been implemented in the config file")
             pass
-        if agent == "GS_cluster_2415878664_254486231_359566_359576":
-            pass
+        # if agent == "256201389":
+        #     pass
         env.step(action)
         done = termination or truncation
 
