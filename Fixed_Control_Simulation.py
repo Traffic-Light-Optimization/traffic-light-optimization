135749562434a51c5211870a2a4667e19e362756
Michael Rolle <michaelrolle7@gmail.com>
2023-09-19 11:56:08 +0200

    Fixed timings now working, no longer have to input number of agents manually

--- Fixed_Control_Simulation.py
+++ Fixed_Control_Simulation.py
@@ -1,4 +1,4 @@
-import sumo_rl
+from sumo_rl.environment.env import SumoEnvironment
 from config_files.greedy.observation import GreedyObservationFunction
 from config_files.greedy.action import greedy_action
 from config_files.max_pressure.observation import MaxPressureObservationFunction
@@ -7,15 +7,15 @@ from config_files.action_lane_relationships import Map_Junction_Action_Lanes
 from config_files.net_route_directories import get_file_locations
 import csv
 
-type = "max_pressure" #greedy, max_pressure, fixed (fixed does not work)
+type = "fixed" #greedy, max_pressure, fixed (fixed does not work)
 map_name = "cologne8" #choose the map to simulate
 map = get_file_locations(map_name) #obtain network, route, and additional files
-gui = False #SUMO gui
+gui = True #SUMO gui
 num_seconds = 3600 #episode duration
 delta_time = 5 #step duration
 action_lanes = Map_Junction_Action_Lanes[map_name] #dict of relationships between actions and lanes for each intersection
 
-env = sumo_rl.env(
+env = SumoEnvironment(
     net_file=map["net"],
     route_file=map["route"],
     use_gui=gui,
@@ -26,45 +26,23 @@ env = sumo_rl.env(
     fixed_ts = True if type == "fixed" else False
 )
 
-# Initialize a list to store the data
-data = []
-
-env.reset()
+data = [] #initialize a list to store the data
+observations = env.reset()
 done = False
-info = ""
-num_agents = 8 # Have to set this manually for now
-count = 1
-step = 0  # Initialize the step variable
-
 while not done:
-    for agent in env.agent_iter():
-        observation, reward, termination, truncation, info = env.last()
-        done = termination or truncation
-        if agent in action_lanes.keys():
-            if type == "greedy":
-                action = greedy_action(observation, action_lanes[agent]) if not done else None
-            else:
-                action = max_pressure_action(observation, action_lanes[agent]) if not done else None
-        else:
-            raise ValueError(f"Agent {agent} has not been implemented in the config file")
-        env.step(action)
+    if type == "greedy":
+        actions = {agent: greedy_action(observations[agent], action_lanes[agent]) for agent in env.ts_ids}
+    elif type == "max_pressure":
+        actions = {agent: max_pressure_action(observations[agent], action_lanes[agent]) for agent in env.ts_ids}
+    elif type == "fixed":
+        actions = {}
+    else:
+        raise ValueError(f"{type} is an invalid type")
+    observations, rewards, dones, infos = env.step(actions)
+    data.append(infos.copy())
+    done = dones['__all__']
 
-        if count % num_agents == 0:
-            # Add the "step" column to the info dictionary
-            info = {'step': step, **info}
-            step += 5  # Increment the step by 5 for the next entry
-
-            # Extract the headings from the info dictionary
-            if data:
-                headings = data[0].keys()
-            else:
-                headings = info.keys()
-
-            # Append the info dictionary to the data list
-            data.append(info.copy())
-
-        count += 1
-
+headings = data[0].keys()
 env.close()
 
 # Create a CSV file and write the data to it
--- MULTIAGENT.md
+++ MULTIAGENT.md
@@ -1,4 +1,5 @@
 # Setup instructions
+
 - pip install optuna
 - pip install optuna-dashboard
 - pip install supersuit
@@ -8,24 +9,34 @@
 # NB!!! Don't forget to replace the files that need to be replaced in your python sumo-rl pip package and add the config_files
 
 # Set parameters
+
 - Replace type with AEC OR Parallel
 - Replace mdl with PPO or DQN
 - Set the parameters at the top of each respective file
+
 # Train instructions
+
 - python Multi-Agent-Tuned-Train.py
- or For parallel computing with multi-cpu's
+  or For parallel computing with multi-cpu's
 - python Multi-Agent-Train.py
 
 # See hyper parameter tuning results
+
 - optuna-dashboard ./optuna/multi-agent-tuned-using-optuma-{type}-{mdl}
 
 # Simulate trained model, view mean reward and compare with random phases
+
 - python Multi-Agent-Simulation
 - Remember to select with model you want to simulate
 
 # Plot graphs
+
 - results are saved in the subplots.pdf file
+
 ## Summary of how the model improves over multi-episodes
+
 - python plot.py -f ./results/train/results-Parallel-PPO_conn1
+
 ## Look at a specific episode
+
 - python plot.py -f ./results/train/results-Parallel-PPO_conn1_ep2
