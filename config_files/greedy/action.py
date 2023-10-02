3342aecf1a6ea5d1dff57bdc753e137f542a008f
Michael Rolle <michaelrolle7@gmail.com>
2023-10-02 15:10:29 +0200

    Updated greedy and max pressure to choose random actions if there is no observation

--- config_files/greedy/action.py
+++ config_files/greedy/action.py
@@ -1,11 +1,16 @@
+import random
+
 def greedy_action(obs: list, action_lane_relationships) -> int:
-    max_queue = -1
+    max_queue = -99999999
     result = None
-    for action, lanes in action_lane_relationships.items():
-        queue = 0
-        for lane in lanes:
-            queue += obs[lane]
-            if queue > max_queue:
-                max_queue = queue
-                result = action
+    if all(observation == 0 for observation in obs):
+        result = random.choice(list(action_lane_relationships.keys()))
+    else:
+        for action, lanes in action_lane_relationships.items():
+            queue = 0
+            for lane in lanes:
+                queue += obs[lane]
+                if queue > max_queue:
+                    max_queue = queue
+                    result = action
     return result
--- config_files/max_pressure/action.py
+++ config_files/max_pressure/action.py
@@ -1,11 +1,16 @@
+import random
+
 def max_pressure_action(obs: list[list[str]], action_lane_relationships) -> int: #obs is list of current vehicle ids in each lane
     max_pressure = -99999999
     result = None
-    for action, lanes in action_lane_relationships.items():
-        pressure = 0
-        for lane in lanes:
-            pressure += obs[lane]
-            if pressure > max_pressure:
-                max_pressure = pressure
-                result = action
+    if all(observation == 0 for observation in obs):
+        result = random.choice(list(action_lane_relationships.keys()))
+    else:
+        for action, lanes in action_lane_relationships.items():
+            pressure = 0
+            for lane in lanes:
+                pressure += obs[lane]
+                if pressure > max_pressure:
+                    max_pressure = pressure
+                    result = action
     return result
