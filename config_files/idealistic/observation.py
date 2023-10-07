21b5d68271d6f1eae3a76af7a4cc022b2a441598
Michael Rolle <michaelrolle7@gmail.com>
2023-10-07 15:49:52 +0200

    Updated the observations to match ob10

--- config_files/idealistic/observation.py
+++ config_files/idealistic/observation.py
@@ -15,24 +15,24 @@ class ModelIdealObservationFunction(ObservationFunction):
         #Incoming lane data
         phase_id = [1 if self.ts.green_phase == i else 0 for i in range(self.ts.num_green_phases)]  # one-hot encoding
         # min_green = [0 if self.ts.time_since_last_phase_change < self.ts.min_green + self.ts.yellow_time else 1]
-        density = self.ts.get_lanes_density() # The density is computed as the number of vehicles divided by the number of vehicles that could fit in the lane.
+        # density = self.ts.get_lanes_density() # The density is computed as the number of vehicles divided by the number of vehicles that could fit in the lane.
         # time_since_last_phase_change = self.ts.get_time_since_last_phase_change()
         queue = self.ts.get_lanes_queue() # The queue is computed as the number of vehicles halting divided by the number of vehicles that could fit in the lane.
-        wait = self.ts.get_accumulated_waiting_time_per_lane() # Returns the accumulated waiting time per lane.
+        # wait = self.ts.get_accumulated_waiting_time_per_lane() # Returns the accumulated waiting time per lane.
         laneOccupancy = self.ts.get_occupancy_per_lane() # Returns the occupancy (20 to 35 meters around the intersection) of each lane
         avgSpeedPerLane = self.ts.get_average_lane_speeds() # returns the average speed of the vehicles in each lane
         # minDist = self.ts.get_dist_to_intersection_per_lane() # returns the distance of the closest car to the intersection for each lane
 
         #Outgoing lane data
         # queueOut = self.ts.get_outgoing_lanes_queue() #returns the number of vehicles halting divided by the total number of vehicles that can fit in the outgoing lanes. This prevents the model from prioritizing phases when the cars are unable to flow through the intersection into the outgoing lanes.
-        observation = np.array(phase_id + queue + wait + laneOccupancy + avgSpeedPerLane + density, dtype=np.float32)
+        observation = np.array(phase_id + queue + laneOccupancy + avgSpeedPerLane, dtype=np.float32)
         return observation
 
     def observation_space(self) -> spaces.Box:
         """Return the observation space."""
         return spaces.Box(
-            low=np.zeros(self.ts.num_green_phases + 5 * len(self.ts.lanes), dtype=np.float32),
-            high=np.ones(self.ts.num_green_phases + 5 * len(self.ts.lanes), dtype=np.float32),
+            low=np.zeros(self.ts.num_green_phases + 3 * len(self.ts.lanes), dtype=np.float32),
+            high=np.ones(self.ts.num_green_phases + 3 * len(self.ts.lanes), dtype=np.float32),
         )
     
 class GreedyIdealObservationFunction(ObservationFunction):
