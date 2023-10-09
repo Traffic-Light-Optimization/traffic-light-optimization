969a4488d3ed48961bccbb4814f79e8e0fe403ab
Michael Rolle <michaelrolle7@gmail.com>
2023-10-09 11:35:28 +0200

    Increased sim repeats to 10 for simulating

--- Multi-Agent-Simulation.py
+++ Multi-Agent-Simulation.py
@@ -19,7 +19,7 @@ from config_files import reward_directories
 numSeconds = 3600 # This parameter determines the total duration of the SUMO traffic simulation in seconds.
 deltaTime = 8 #This parameter determines how much time in the simulation passes with each step.
 max_green = 60
-simRepeats = 1 # Number of episodes
+simRepeats = 10 # Number of episodes
 parallelEnv = 1
 num_cpus = 1
 maps = ['ingolstadt1', 'ingolstadt7', 'ingolstadt21']
