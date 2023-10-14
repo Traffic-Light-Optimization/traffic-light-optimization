b8edb306c3ffc24c535509f19ac8f8e17bbad15d
Bryce <99645139+bcgrahn@users.noreply.github.com>
2023-10-14 12:52:31 +0200

    fix tune naming scheme

--- tune-rename.py
+++ tune-rename.py
@@ -5,12 +5,19 @@ import re
 def rename_files_in_folder(folder_path):
     for filename in os.listdir(folder_path):
         old_file_path = os.path.join(folder_path, filename)
-        
+
         if os.path.isfile(old_file_path):
-            new_filename = re.sub(r'<.*>', '', filename)  # Remove text within <>
-            new_file_path = os.path.join(folder_path, new_filename)
-            os.rename(old_file_path, new_file_path)
-            print(f"Renamed: {filename} to {new_filename}")
+            # Extract the text within <...>
+            match = re.search(r'<(.*?)>', filename)
+            if match:
+                text_inside_brackets = match.group(1)
+                # Get the last 12 characters within the brackets
+                new_text = text_inside_brackets[-12:]
+                # Remove <...> brackets and replace with the new text
+                new_filename = re.sub(r'<(.*?)>', new_text, filename)
+                new_file_path = os.path.join(folder_path, new_filename)
+                os.rename(old_file_path, new_file_path)
+                print(f"Renamed: {filename} to {new_filename}")
 
 def main():
     parser = argparse.ArgumentParser(description="Rename files in a folder.")
