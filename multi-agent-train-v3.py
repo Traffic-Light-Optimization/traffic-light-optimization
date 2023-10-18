c7f487d6e2daaa0efb31a75d715194a212c0ac20
Bryce <99645139+bcgrahn@users.noreply.github.com>
2023-10-18 18:58:04 +0200

    update v3

--- multi-agent-train-v3.py
+++ multi-agent-train-v3.py
@@ -20,8 +20,8 @@ from config_files import reward_directories
 numSeconds = 3600 # This parameter determines the total duration of the SUMO traffic simulation in seconds.
 deltaTime = 8 #This parameter determines how much time in the simulation passes with each step.
 max_green = 60
-simRepeats = 50 # Number of episodes
-parallelEnv = 14
+simRepeats = 55 # Number of episodes
+parallelEnv = 16
 # evaluation_interval = 500 #How many seconds in you want to evaluate the model that is being trained to save the best one
 num_cpus = 8
 yellow_time = 3 # min yellow time
